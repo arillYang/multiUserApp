cbedce7eba575ff09d22cba91009bb36b3f87b8e
PanYin <1045524818@qq.com>
2018-11-02 14:05:37 +0800

    全球发现

---
+++ src/main/java/com/xuyang/controller/Tglobalfind.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C), 2018-2018, 杭州旭阳科技有限公司
+ * FileName: Tglobalfind
+ * Author:   PanYin
+ * Date:     2018/11/2 11:48
+ * Description: 全球发现
+ */
+package com.xuyang.controller;
+
+import com.xuyang.mapper.TdynamicMapper;
+import com.xuyang.model.Tdynamic;
+import com.xuyang.model.TdynamicExample;
+import com.xuyang.model.TgtypeExample;
+import com.xuyang.util.ResultConstant;
+import com.xuyang.util.XuYangResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈全球发现〉
+ * @author PanYin
+ * @create 2018/11/2
+ * @since 1.0.0
+ */
+@Api(description = "全球发现")
+@RestController
+@RequestMapping(value = "/globalfind")
+public class Tglobalfind {
+
+    @Autowired
+    private TdynamicMapper mapper;
+
+    @ApiOperation(value = "查询动态")
+    @RequestMapping(value = "/queryDynamic",produces = {"application/json;charset=UTF-8"},method = RequestMethod.GET)
+    @ResponseBody
+    public Object queryDynamic(){
+        TdynamicExample example=new TdynamicExample();
+        TdynamicExample.Criteria criteria = example.createCriteria();
+        criteria.andDyIdIsNotNull();
+        List<Tdynamic> tdynamics = mapper.selectByExample(example);
+        return XuYangResult.ok(ResultConstant.code_ok,"成功",tdynamics);
+    }
+}
---
+++ src/main/java/com/xuyang/controller/TgroupdiscountController.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C), 2018-2018, 杭州旭阳科技有限公司
+ * FileName: TgroupdiscountController
+ * Author:   PanYin
+ * Date:     2018/11/2 10:38
+ * Description: 团购优惠
+ */
+package com.xuyang.controller;
+
+import com.xuyang.mapper.TgtypeMapper;
+import com.xuyang.model.Tgoods;
+import com.xuyang.model.Tgtype;
+import com.xuyang.model.TgtypeExample;
+import com.xuyang.service.TgoodsService;
+import com.xuyang.util.ResultConstant;
+import com.xuyang.util.XuYangResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈团购优惠〉
+ * @author PanYin
+ * @create 2018/11/2
+ * @since 1.0.0
+ */
+@Api(description = "团购优惠")
+@RestController
+@RequestMapping(value = "/group")
+public class TgroupdiscountController {
+    //商品类型service
+    @Autowired
+    private TgtypeMapper tgtypeMapper;
+    //商品service
+    @Autowired
+    private TgoodsService tgoodsService;
+
+    @ApiOperation(value = "查询类型")
+    @RequestMapping(value = "/queryType",produces = {"application/json;charset=UTF-8"},method = RequestMethod.GET)
+    @ResponseBody
+    public Object queryGroupType(){
+        TgtypeExample example=new TgtypeExample();
+        TgtypeExample.Criteria criteria = example.createCriteria();
+        criteria.andGtIdIsNotNull();
+        List<Tgtype> tgtypes = tgtypeMapper.selectByExample(example);
+        return XuYangResult.ok(ResultConstant.code_ok,"",tgtypes);
+    }
+
+    @ApiOperation(value = "查询商品")
+    @ResponseBody
+    @RequestMapping(value = "/bestArrivals",method = RequestMethod.GET)
+    public Object queryBestGoos(){
+        List<Tgoods> tgoods = tgoodsService.queryNewGoods();
+        if(tgoods == null){
+            return XuYangResult.ok(ResultConstant.code_failue,"没有数据",null);
+        }else{
+            return XuYangResult.ok(ResultConstant.code_ok,"成功",tgoods);
+        }
+    }
+
+
+}
